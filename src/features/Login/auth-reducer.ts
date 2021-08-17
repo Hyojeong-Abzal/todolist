24d646ff340835a25b3c2a44ce988cdb29e85b65
suan <abzal008@mail.ru>
2021-08-17 23:04:15 +0300

    add slice to tasks reducer

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -26,13 +26,13 @@ export const { setIsLoggedInAC } = slice.actions
 
 // thunks
 export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC('loading'))
+  dispatch(setAppStatusAC({status: 'loading'}))
   authAPI
     .login(data)
     .then(res => {
       if (res.data.resultCode === 0) {
         dispatch(setIsLoggedInAC({ value: true }))
-        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setAppStatusAC({status: 'succeeded'}))
       } else {
         handleServerAppError(res.data, dispatch)
       }
@@ -42,13 +42,13 @@ export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
     })
 }
 export const logoutTC = () => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC('loading'))
+  dispatch(setAppStatusAC({ status: 'loading'}))
   authAPI
     .logout()
     .then(res => {
       if (res.data.resultCode === 0) {
         dispatch(setIsLoggedInAC({ value: false }))
-        dispatch(setAppStatusAC('succeeded'))
+        dispatch(setAppStatusAC({status: 'succeeded'} ))
       } else {
         handleServerAppError(res.data, dispatch)
       }
